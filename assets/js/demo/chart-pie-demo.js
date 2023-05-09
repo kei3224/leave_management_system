46106b280c669131d7a20eddeeeff0b4ebee89c1
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2023-05-09 13:57:41 +0800

    readme commit

--- assets/js/demo/chart-pie-demo.js
+++ assets/js/demo/chart-pie-demo.js
@@ -4,7 +4,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
-        labels: ["Direct", "Referral", "Social"],
+        labels: ["Pending", "Approved", "Denied"],
         datasets: [{
             data: [55, 30, 15],
             backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
