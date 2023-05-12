a245123ca65470dfdc35fe45b4d15b92742243db
kei3224 <krrtablang_19ac0055@psu.edu.ph>
2023-05-12 09:30:22 +0800

    generate pdf commit

--- generate_pdf.php
+++ generate_pdf.php
@@ -1,20 +1,43 @@
 <?php
-// if (isset($_GET['id']) && $_GET['id'] > 0) {
-//     $qry = $conn->query("SELECT * from `leave_applications` where id = '{$_GET['id']}' ");
-//     if ($qry->num_rows > 0) {
-//         foreach ($qry->fetch_assoc() as $k => $v) {
-//             $$k = $v;
-//         }
-//     }
-// }
-// if ($_settings->userdata('type') == 3) {
-//     $meta_qry = $conn->query("SELECT * FROM employee_meta where meta_field = 'leave_type_ids' and user_id = '{$_settings->userdata('id')}' ");
-//     $leave_type_ids = $meta_qry->num_rows > 0 ? $meta_qry->fetch_array()['meta_value'] : '';
-// }
+if (isset($_GET['id']) && $_GET['id'] > 0) {
+    $qry = $conn->query("SELECT * from `leave_applications` where id = '{$_GET['id']}' ");
+    if ($qry->num_rows > 0) {
+        foreach ($qry->fetch_assoc() as $k => $v) {
+            $$k = $v;
+        }
+    }
+}
+if ($_settings->userdata('type') == 3) {
+    $meta_qry = $conn->query("SELECT * FROM employee_meta where meta_field = 'leave_type_ids' and user_id = '{$_settings->userdata('id')}' ");
+    $leave_type_ids = $meta_qry->num_rows > 0 ? $meta_qry->fetch_array()['meta_value'] : '';
+}
 ?>
 <?php
-ob_start();
+// Include the main TCPDF library (search for installation path).
+ob_clean();
 require_once('library/tcpdf.php');
+//  // fetching the data and insertiong into the pdf
+//  if (isset($_GET['id']) && $_GET['id'] > 0) {
+//      $qry = $conn->query("SELECT l.*,concat(u.lastname,' ',u.firstname,' ',u.middlename) as `name`,lt.code,lt.name as lname from `leave_applications` l inner join `users` u on l.user_id=u.id inner join `leave_types` lt on lt.id = l.leave_type_id  where l.id = '{$_GET['id']}' ");
+//      //  if ($qry->num_rows > 0) {
+//      //      foreach ($qry->fetch_assoc() as $k => $v) {
+//      //          $$k = $v;
+//      //      }
+//      //  }
+//      $lt_qry = $conn->query("SELECT meta_value FROM `employee_meta` where user_id = '{$user_id}' and meta_field = 'employee_id' ");
+//      $employee_id = ($lt_qry->num_rows > 0) ? $lt_qry->fetch_array()['meta_value'] : "N/A";
+//  }
+//    $config_path =  "config.php";
+//    // Use the "require_once" statement to include the file
+//    if (file_exists($config_path)) {
+//        require_once $config_path;
+//        echo "Executed";
+//       // Continue executing the script
+//    } else {
+//        // If the file does not exist, output an error message and exit the script
+//        echo "Error: could not include file {$config_path}";
+//        exit;
+//    }
 class PDF extends TCPDF
 {
     public function Header()
@@ -36,12 +59,12 @@ class PDF extends TCPDF
         $this->SetFont('helvetica', 'BI', 6);
         $this->Cell(0, 3, '', 0, 1, 'C');
         $this->SetFont('helvetica', 'B', 16);
-        $this->Cell(0, 3, 'APPLICATION FOR LEAVE', 0, 1, 'C');
+        $this->Cell(0, 3, 'APPLICATION FOR LEAVE', 0, 1, 'C'); 
         $this->SetFont('helvetica', 'BI', 6);
         $this->Cell(0, 3, '', 0, 1, 'C');
 
         //adding the contents
-        $this->SetFont('helvetica', '', 10);
+        $this->SetFont('helvetica', '', 9);
         $this->Cell(202, 5, ' 1. OFFICE DEPARTMENT                      2. NAME:             (Last)                               (First)                             (Middle)', 'LTR', 1, '');
         $this->Cell(202, 8, '      ', 'LRB', 1, '');
 
@@ -55,10 +78,9 @@ class PDF extends TCPDF
         $this->Cell(202, 0, ' ', 1, 1, 'C');
 
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(115, 8, " 6.A TYPE OF LEAVE TO BE AVAILED OF    ", 'TLR', );
+        $this->Cell(115, 8, " 6.A TYPE OF LEAVE TO BE AVAILED OF    ", 'TLR',);
         $this->Cell(87, 8, " 6.B DETAILS OF LEAVE", 'TLR');
         $this->Ln();
-
 
         $this->SetFont('dejavusans', '', 14);
         $this->Cell(6, 6, ' ☐', 'L');
@@ -141,7 +163,7 @@ class PDF extends TCPDF
         $this->SetFont('dejavusans', '', 14);
         $this->Cell(6, 6, ' ☐', 'L');
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(21, 6, ' Study Leave ', '');
+        $this->Cell(21 , 6, ' Study Leave ', '');
         $this->SetFont('helvetica', '', 6);
         $this->Cell(88, 6, ' (Sec. 68, Rule XVI, Omnibus Rules Implementing E.O. No. 292)', 'R');
         $this->SetFont('helvetica', 'I', 10);
@@ -248,14 +270,14 @@ class PDF extends TCPDF
         $this->SetFont('helvetica', 'I', 10);
         $this->Cell(115, 6, '      ________________________________________', 'LR');
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(87, 6, "    ________________________________________", 'LR');
+        $this->Cell(87, 0, "    ________________________________________", 'LR');
         $this->Ln();
 
         $this->SetFont('helvetica', 'I', 10);
         $this->Cell(115, 6, '      ', 'LR');
-        $this->Cell(87, 6, "       ", 'LR');
+        $this->Cell(87, 6, "                         (Signature of Applicant)", 'LR');
         $this->Ln();
-
+        
         $this->SetFont('helvetica', '', 1);
         $this->Cell(202, 0, ' ', 1, 1, 'C');
         $this->SetFont('helvetica', 'B', 12);
@@ -281,47 +303,47 @@ class PDF extends TCPDF
 
         $this->SetFont('helvetica', 'I', 10);
         $this->Cell(6, 6, '', 'LR');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(34, 6, '     Vacation Leave', 'TLRB');
-        $this->Cell(34, 6, '        Sick Leave', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(34, 2, '     Vacation Leave', 'TLRB');
+        $this->Cell(34, 2, '        Sick Leave', 'TLRB');
         $this->Cell(7, 6, '', 'LR');
 
         $this->SetFont('dejavusans', '', 14);
-        $this->Cell(9, 6, '   ☐', 'L');
+        $this->Cell(9, 2, '   ☐', 'L');
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(78, 6, " For disapproval due to __________________", 'R');
+        $this->Cell(78, 2, " For disapproval due to __________________", 'R');
         $this->Ln();
 
         $this->SetFont('helvetica', 'I', 10);
-        $this->Cell(6, 6, '', 'LR');
-        $this->Cell(34, 6, '      Total Earned', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(7, 6, '', 'LR');
-        $this->Cell(87, 6, "         _____________________________________", 'LR');
+        $this->Cell(6, 2, '', 'LR');
+        $this->Cell(34, 2, '      Total Earned', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(7, 2, '', 'LR');
+        $this->Cell(87, 2, "         _____________________________________", 'LR');
         $this->Ln();
 
         $this->SetFont('helvetica', 'I', 10);
-        $this->Cell(6, 6, '', 'LR');
-        $this->Cell(34, 6, ' Less this Application', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(7, 6, '', 'LR');
-        $this->Cell(87, 6, "         _____________________________________", 'LR');
+        $this->Cell(6, 2, '', 'LR');
+        $this->Cell(34, 2, ' Less this Application', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(7, 2, '', 'LR');
+        $this->Cell(87, 2, "         _____________________________________", 'LR');
         $this->Ln();
 
         $this->SetFont('helvetica', 'I', 10);
-        $this->Cell(6, 6, '', 'LR');
-        $this->Cell(34, 6, '          Balance', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(34, 6, '   ', 'TLRB');
-        $this->Cell(7, 6, '', 'LR');
-        $this->Cell(87, 6, "         _____________________________________", 'LR');
+        $this->Cell(6, 2, '', 'LR');
+        $this->Cell(34, 2, '          Balance', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(34, 2, '   ', 'TLRB');
+        $this->Cell(7, 2, '', 'LR');
+        $this->Cell(87, 2, "         _____________________________________", 'LR');
         $this->Ln();
 
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(115, 6, '', 'LR');
-        $this->Cell(87, 6, "", 'LR');
+        $this->Cell(115, 2, '', 'LR');
+        $this->Cell(87, 2, "", 'LR');
         $this->Ln();
 
         $this->SetFont('helvetica', 'I', 10);
@@ -346,18 +368,18 @@ class PDF extends TCPDF
         $this->Ln();
 
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(115, 6, '      ___________ days with pay', 'L');
-        $this->Cell(87, 6, "         _____________________________________", 'R');
+        $this->Cell(115, 2, '      ___________ days with pay', 'L');
+        $this->Cell(87, 2, "         _____________________________________", 'R');
         $this->Ln();
 
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(115, 6, '      ___________ days without pay', 'L');
-        $this->Cell(87, 6, "         _____________________________________", 'R');
+        $this->Cell(115, 2, '      ___________ days without pay', 'L');
+        $this->Cell(87, 2, "         _____________________________________", 'R');
         $this->Ln();
 
         $this->SetFont('helvetica', '', 10);
-        $this->Cell(115, 6, '      ___________ others specify', 'L');
-        $this->Cell(87, 6, "         _____________________________________", 'R');
+        $this->Cell(115, 2, '      ___________ others specify', 'L');
+        $this->Cell(87, 2, "         _____________________________________", 'R');
         $this->Ln();
 
         $this->Cell(115, 6, '', 'L');
@@ -376,6 +398,12 @@ class PDF extends TCPDF
 
 
     }
+
+    public function Footer()
+    {
+
+    }
+
 }
 
 // create new PDF document
@@ -385,8 +413,8 @@ $pdf = new PDF('p', 'mm', 'FOLIO', true, 'UTF-8', false);
 $pdf->SetCreator(PDF_CREATOR);
 $pdf->SetAuthor('Department of Education');
 $pdf->SetTitle('APPLICATION FOR LEAVE');
-$pdf->SetSubject('Generated Application for Leave');
-// $pdf->SetKeywords('');
+$pdf->SetSubject('');
+$pdf->SetKeywords('');
 
 // set default header data
 $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE . ' 001', PDF_HEADER_STRING, array(0, 64, 255), array(0, 64, 128));
@@ -409,7 +437,7 @@ $pdf->Write(10, "\xE2\x98\x91"); /////////
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 // set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+// $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 $pdf->SetMargins(7, 0, 7, true);
 $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
 $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
@@ -443,20 +471,18 @@ $pdf->AddPage();
 
 // set text shadow effect
 $pdf->setTextShadow(array('enabled' => true, 'depth_w' => 0.2, 'depth_h' => 0.2, 'color' => array(196, 196, 196), 'opacity' => 1, 'blend_mode' => 'Normal'));
-ob_start();
-?>
-<!-- Set some html contents -->
-<?php
-$html = ob_get_clean();
 
-// output the HTML content
-$pdf->writeHTML($html, true, false, true, false, '');
+// Set some content to print
 
-// $pdf->writeHTML($html, $ln=true, $fill=false, $reseth=false, $cell=false, $align='');
-// writeHTMLCell($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=0, $reseth=true, $align='', $autopadding=true);
 
+
+// ---------------------------------------------------------
 
 // Close and output PDF document
 // This method has several options, check the source code documentation for more information.
-$pdf->Output();
-unset($pdf);
+$pdf->Output('leave.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
+?>
